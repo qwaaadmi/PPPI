731fe573ae81e71887a6cd702a981df40dbbedd2
qwaaadmi <st9334954@stud.duikt.edu.ua>
2026-01-24 13:59:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import express from "express";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 app.get("/", (req, res) => {
-  res.send("Server is working!");
+  res.send("Server is working with Socket.IO!");
+});
+
+io.on("connection", socket => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
